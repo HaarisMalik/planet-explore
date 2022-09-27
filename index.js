129a6fbef35f0a8391d2e643b1ba9744aa5a0440
HaarisMalik <haaris.malik416@gmail.com>
2022-09-27 08:34:27 -0700

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// PARALLAX
+
+let scene = document.getElementById("scene")
+let parallaxInstance = new Parallax(scene)
+
+let keys = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune"
+  ];
+  let slider = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 150,
+    centeredSlides: true,
+    mousewheel: true,
+    pagination: {
+      el: ".planet-links",
+      clickable: true,
+      renderBullet: function(index, className) {
+        return '<div class="' + className + '">' + keys[index] + "</div>";
+      }
+    }
+  });
+
+
+  
+ 
+  
+  
