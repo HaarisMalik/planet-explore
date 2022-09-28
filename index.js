6b39fe9b03a2a75a0e8f4f9ce12eefd0c686dd50
HaarisMalik <haaris.malik416@gmail.com>
2022-09-28 09:57:51 -0700

    animated/style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// PARALLAX
+// PARALLAX HERO SECTION
 
 let scene = document.getElementById("scene")
 let parallaxInstance = new Parallax(scene)
@@ -27,8 +27,23 @@ let keys = [
     }
   });
 
+  // PARALLAX SECOND SECTION
 
-  
+  let stars = document.getElementById('stars');
+  let moon = document.getElementById('moon');
+  let mountains_behind = document.getElementById('mountains-behind');
+  let text2 = document.getElementById('text2');
+  let mountains_front = document.getElementById('mountains-front');
+
+  window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 0.30 + 'px';
+    mountains_behind.style.top = value * 0 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text2.style.marginRight = value * 0.5 + 'px';
+    text2.style.marginTop = value * 0.2 + 'px';
+  })
  
   
   
