f2c18dca27ad4b7cee8b5a200e01032722e7a130
HaarisMalik <haaris.malik416@gmail.com>
2022-10-05 07:27:55 -0700

    animated/scroll/website

--- index.js
+++ index.js
@@ -24,21 +24,7 @@ let parallaxInstance = new Parallax(scene)
     //text2.style.marginTop = value * 0.2 + 'px';
   })
 
-  // Scroll through animation
 
-  const observer = new IntersectionObserver((entries) => {
-       entries.forEach((entry) => {
-         console.log(entry)
-         if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-         } else {
-          entry.target.classList.remove('show');
-         }
-       });
-  });
-
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
  
   
   
